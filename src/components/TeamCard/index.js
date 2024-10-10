12f71e146bac13766bcf773030bb55b58459bc1b
jashu <rachapallijaswanthreddy@gmail.com>
2024-10-10 15:48:24 +0000

    IPL Dashboard

--- src/components/TeamCard/index.js
+++ src/components/TeamCard/index.js
@@ -1,23 +1,20 @@
 // Write your code here
 import {Link} from 'react-router-dom'
 
-import {Component} from 'react'
-
 import './index.css'
 
-class TeamCard extends Component {
-  render() {
-    const {teamData} = this.props
-    const {name, imageUrl, id} = teamData
-    return (
-      <Link to={`/team-matches/${id}`} className="link-item">
-        <li className="team-card">
-          <img className="team-card-image" src={imageUrl} alt={`${name}`} />
-          <p className="team-card-name">{name}</p>
-        </li>
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {name, id, teamImageURL} = teamDetails
+
+  return (
+    <li className="team-item">
+      <Link to={`/team-matches/${id}`} className="link">
+        <img src={teamImageURL} alt={name} className="team-logo" />
+        <p className="team-name">{name}</p>
       </Link>
-    )
-  }
+    </li>
+  )
 }
 
 export default TeamCard
